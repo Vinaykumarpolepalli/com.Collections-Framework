c02340934a464206772c25799f845dfce174fb17
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2025-04-12 22:45:13 +0530

    Add files via upload

---
+++ Collectiomap1.java
@@ -0,0 +1,43 @@
+package com.Collections;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class Collectiomap1 {
+
+	public static void main(String[] args) {
+		Map <Integer ,String> m = new HashMap<>();
+		m.put(30, "Vinay");
+		m.put(18, "Virat");
+		m.put(7, "MSD");
+		//m.put(30, "kumar");//Updated
+		System.out.println(m.put(21, "Guru"));//Null
+		System.out.println(m.put(21, "sai"));//Guru
+		
+		System.out.println(m.containsKey(18));//True
+		System.out.println(m.containsValue("virat"));//False Because of the virat v is small Letter 
+		
+		//Represent only key
+		System.out.println(m.keySet());//[8, 21, 7, 30]/
+		//Represent only values
+		System.out.println(m.values());//[Virat, Vinay, MSD, Vinay]
+		
+		m.remove(30);
+		
+		//Represent the key and values
+		Set<Entry<Integer,String>> enteries=m.entrySet();
+		for(Entry<Integer,String> entry:enteries) {
+			System.out.println(entry.getKey()+":"+entry.getValue());
+		}
+		
+//		Map <String,String> m1 = new HashMap<>();
+//		m1.put("SRH","Hyderdad");
+//		System.out.println(m);
+//		System.out.println(m1);
+//		
+		
+	}
+
+}
